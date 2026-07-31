2428e9d304e236efb521bf29f10e83b028ae89cf
visurainfotech-svg <visurainfotech@gmail.com>
2026-07-31 10:54:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -97,4 +97,10 @@ Message:
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=False)
+    import os
+
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False
+    )
